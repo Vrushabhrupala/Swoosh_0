44008088ae47bd5467b56f754c0aba4c20719b40
vrushabh rupala <rupalavrushabh54@gmail.com>
2020-07-30 17:24:12 +0530

    added vc files and created 2nd vc => 3rd vc

---
+++ Swoosh_0/Model/Player.swift
@@ -0,0 +1,15 @@
+
+//
+//  File.swift
+//  Swoosh_0
+//
+//  Created by Vrushabh Rupala on 30/07/20.
+//  Copyright © 2020 Vrushabh Rupala. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
